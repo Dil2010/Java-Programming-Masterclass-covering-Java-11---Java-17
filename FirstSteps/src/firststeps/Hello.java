bcb6d9967bb466c8cbf1c9afd42286f711cc20b6
Dilan Udawattha <87587934+Dil2010@users.noreply.github.com>
2022-08-02 09:53:50 -0700

    Updates

--- FirstSteps/src/firststeps/Hello.java
+++ FirstSteps/src/firststeps/Hello.java
@@ -1,4 +1,13 @@
 package firststeps;
 
 public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int myFirstNumber=5;
+        System.out.println("My first number is  "+myFirstNumber);
+        myFirstNumber=myFirstNumber+5;
+        System.out.println("My next number is   "+myFirstNumber);
+    }
 }
